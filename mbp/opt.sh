f8e95c1f5ce6056b0e28195bfac6c5bba54ab1e0
prahlad01 <43715907+prahlad01@users.noreply.github.com>
2020-07-21 13:25:12 +0530

    Update opt.sh

--- mbp/opt.sh
+++ mbp/opt.sh
@@ -5,7 +5,7 @@ grn='\033[1;32m'
 ylo='\033[1;33m'
 #script coding starts
 clear
-cd $HOME/m-wiz/mbp
+cd $HOME/hjacker/mbp
 echo " "
 echo " "
 echo -e "$ylo      To terminate the process click 'n' or to continue
@@ -14,11 +14,11 @@ read choice
 if [[ $choice = 'n' ]] ; then
 echo -e "$red    process is terminating? press ENTER to Terminate$rset"
 read choice
-cd $HOME/m-wiz
-bash m-wiz.sh
+cd $HOME/hjacker
+bash hjacker.sh
 else
 read choice
-cd $HOME/m-wiz/min
+cd $HOME/hjacker/min
 bash metain.sh
 echo " "
 echo " "
